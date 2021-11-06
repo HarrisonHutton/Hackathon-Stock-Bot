d451e9fe66256c0f13ca24e3bab018db67803060
HarrisonHutton <harrison.hutton6@gmail.com>
2021-11-06 13:42:43 -0400

    Created cogs file to store command classes; created Market class

---
+++ cogs/market.py
@@ -0,0 +1,16 @@
+"""
+
+This file will store all stock market commands
+
+"""
+
+import discord
+
+from discord.ext import commands
+
+class Market(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # TODO Write market commands here
