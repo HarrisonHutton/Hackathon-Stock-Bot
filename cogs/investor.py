539085dda3587906d5d11e3c9b8013b7a4457ae4
edwiniri <>
2021-11-06 23:57:48 +0000

    added investor class

---
+++ cogs/investor.py
@@ -0,0 +1,15 @@
+"""
+
+This file will store all data in portfolio
+
+"""
+
+import discord
+
+from discord.ext import commands
+class Investor(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # TODO Write investor commands here
