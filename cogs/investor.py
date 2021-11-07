217d157cbfb8edaf2c935c2585269c1b12333906
Alex Eastman <alexeast@buffalo.edu>
2021-11-06 20:06:36 -0400

    Added TODO comment and mandatory setup() function

--- cogs/investor.py
+++ cogs/investor.py
@@ -7,9 +7,14 @@ This file will store all data in portfolio
 import discord
 
 from discord.ext import commands
+
 class Investor(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-    # TODO Write investor commands here
+    # TODO: Data members and methods
+
+
+def setup(client):
+    client.add_cog(Investor(client))
